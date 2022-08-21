93837ff486ffa0e9be6fb59f56c85c1ba04f25e5
Mateleo <mateleo78@gmail.com>
2022-08-21 12:34:22 +0200

    first commit

---
+++ site/src/components/Header.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="fixed top-0 left-0 right-0 z-10">
+    <header
+      class="flex justify-between w-full border-b-[1px] border-white/20 px-4 py-3 items-center backdrop-blur-[2px] text-white bg-[#1C1C1C]/70"
+    >
+      <div class="flex items-center">
+        <div>
+          <h1 class="text-2xl mr-3">4eSport</h1>
+        </div>
+        <nav>
+          <router-link
+            to="/create"
+            class="md:mr-2 mr-1 link-underline hover:text-[#15c584] transition-all ease-in-out delay-75"
+          >
+            Evènements</router-link
+          >
+          <router-link
+            to="/support"
+            class="md:mx-2 mx-1 link-underline hover:text-[#15c584] transition-all ease-in-out delay-75"
+            >Projets</router-link
+          >
+          <router-link
+            to="/mypanel"
+            class="md:ml-2 ml-1 link-underline hover:text-[#15c584] transition-all ease-in-out delay-75"
+            >Palmarès</router-link
+          >
+        </nav>
+      </div>
+      <div>
+        <button>Github</button>
+        <router-link
+          to="/mypanel"
+          class="md:ml-2 ml-1 link-underline hover:text-[#15c584] transition-all ease-in-out delay-75"
+          >Login</router-link
+        >
+      </div>
+    </header>
+  </div>
+</template>
+
+<style>
+.link-underline {
+  border-bottom-width: 0;
+  background-image: linear-gradient(transparent, transparent),
+    linear-gradient(rgb(21, 197, 132), rgb(21, 197, 132));
+  background-size: 0 2px;
+  background-position: 0 100%;
+  background-repeat: no-repeat;
+}
+
+.link-underline:hover {
+  background-size: 100% 2px;
+  background-position: 0 100%;
+}
+</style>
---
+++ site/src/views/HomeView.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import Header from "../components/Header.vue";
+</script>
+
+<template>
+  <Header></Header>
+  <main class="mt-[100px]">
+    <div class="w-[90%] m-auto">
+      <main class="flex m-auto mt-10">
+        <div class="w-1/2 mr-6">
+          <h1 class="text-3xl font-semibold">L'association 100% en ligne.</h1>
+          <h1 class="text-3xl font-semibold text-[#15c584]">Pour tous, par tous.</h1>
+          <p class="mt-5 text-lg text-white/70">
+            4eSport est l’association de jeux vidéo de l'Efrei. Elle regroupe en son sein
+            une multitude de pôles regroupant l’ensemble des jeux esportifs du moment.
+            Elle est ouverte à tous sans condition
+          </p>
+        </div>
+        <div class="">
+          <img src="https://picsum.photos/400" alt="" />
+        </div>
+      </main>
+      <div>
+        <h2 class="font-semibold text-2xl mt-10 mb-5">Nos forces</h2>
+        <div class="flex justify-between">
+          <div class="flex flex-col mr-8">
+            <h3 class="font-semibold">Communauté</h3>
+            <p class="text-white/70">4eSport possède une commaunuté inégalée.</p>
+          </div>
+          <div class="flex flex-col mr-8">
+            <h3 class="font-semibold">Online</h3>
+            <p class="text-white/70">De part sa nature, l'association est ouverte 24h/24h, 7j/7</p>
+          </div>
+          <div class="flex flex-col mr-8">
+            <h3 class="font-semibold">Online</h3>
+            <p class="text-white/70">De part sa nature, l'association est ouverte 24h/24h, 7j/7</p>
+          </div>
+          <div class="flex flex-col">
+            <h3 class="font-semibold">Online</h3>
+            <p class="text-white/70">De part sa nature, l'association est ouverte 24h/24h, 7j/7</p>
+          </div>
+        </div>
+        <div class="h-[1000px]"></div>
+      </div>
+    </div>
+  </main>
+</template>
