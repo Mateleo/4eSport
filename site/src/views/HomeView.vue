d0cd7fff8965ecb60bd90a0f3356f31787e57250
Mateleo <mateleo78@gmail.com>
2022-08-22 10:13:48 +0200

    Nouvelle image de bordure et Responsive

--- site/src/views/HomeView.vue
+++ site/src/views/HomeView.vue
@@ -4,18 +4,30 @@ import Header from "../components/Header.vue";
 
 <template>
   <Header></Header>
-  <main class="mt-[150px]">
+  <main class="sm:mt-[150px] mt-[70px]">
     <div class="w-[95%] md:w-[90%] lg:w-[80%] m-auto">
-      <main class="flex m-auto mt-10 justify-between">
-        <div class="w-full sm:w-1/2 sm:mr-6 flex flex-col">
-          <h1 class="text-2xl md:text-3xl font-semibold">L'association 100% en ligne.</h1>
-          <h1 class="text-2xl md:text-3xl font-semibold text-[#15c584]">Pour tous, par tous.</h1>
-          <p class="mt-5 text-base md:text-lg text-white/70 text-justify md:text-left">
-            4eSport est l’association de jeux vidéo de l'Efrei. Elle regroupe en son sein
-            une multitude de pôles regroupant l’ensemble des jeux esportifs du moment.
-            Elle est ouverte à tous sans condition.
-          </p>
-          <div class="w-full flex justify-center mt-5">
+      <h1 class="font-semibold text-5xl w-full text-center sm:hidden block">4eSport</h1>
+      <main class="flex sm:flex-row flex-col m-auto mt-10 justify-between">
+        <div class="w-full flex flex-wrap flex-col sm:h-[400px]">
+          <div class="sm:w-1/2 sm:pr-5">
+            <h1 class="text-2xl md:text-3xl font-semibold">
+              L'association 100% en ligne.
+            </h1>
+            <h1 class="text-2xl md:text-3xl font-semibold text-[#15c584]">
+              Pour tous, par tous.
+            </h1>
+            <p class="mt-5 text-base md:text-lg text-white/70 text-justify md:text-left">
+              4eSport est l’association de jeux vidéo de l'Efrei. Elle regroupe en son
+              sein une multitude de pôles regroupant l’ensemble des jeux esportifs du
+              moment. Elle est ouverte à tous sans condition.
+            </p>
+          </div>
+          <div class="items-center sm:w-1/2 flex sm:pl-5 mt-5 sm:mt-0 sm:order-1 max-w-[600px]">
+            <div>
+              <img src="../assets/ESL.jpg" alt="" />
+            </div>
+          </div>
+          <div class="w-full flex justify-center mt-5 sm:w-1/2 sm:pr-5">
             <a
               class="flex px-2 py-1 rounded-lg border-[1px] border-[white]/5 bg-[#2c15c5]/70 hover:bg-[#2c15c5] shadow-sm shadow-black/40"
               href="https://discord.gg/qEHEMyadrr"
@@ -23,11 +35,6 @@ import Header from "../components/Header.vue";
             >
               <p class="text-lg font-semibold">Rejoins nous !</p>
             </a>
-          </div>
-        </div>
-        <div class="items-center w-1/2 hidden sm:flex">
-          <div>
-            <img src="https://picsum.photos/800" alt="" />
           </div>
         </div>
       </main>
