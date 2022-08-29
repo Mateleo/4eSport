a893a89d9f68f2a8aaeb5775f8e9e5aab0c613b1
Mateleo <mateleo78@gmail.com>
2022-08-29 11:16:06 +0200

    Stats sur la page d'accueil

--- site/src/views/HomeView.vue
+++ site/src/views/HomeView.vue
@@ -55,23 +55,23 @@ import Footer from "../components/Footer.vue";
           class="sm:flex sm:justify-between grid grid-cols-2 gap-2 md:gap-6 lg:gap-20 gap-y-6"
         >
           <div class="flex flex-col sm:mr-8">
-            <h3 class="font-semibold text-xl">Communauté</h3>
+            <h3 class="font-semibold sm:text-xl text-lg">Communauté</h3>
             <p class="text-white/70 mt-2">4eSport possède une commaunuté inégalée.</p>
           </div>
           <div class="flex flex-col sm:mr-8">
-            <h3 class="font-semibold text-xl">Online</h3>
+            <h3 class="font-semibold sm:text-xl text-lg">Online</h3>
             <p class="text-white/70 mt-2">
               De par sa nature, l'association est ouverte 24h/24h, 7j/7.
             </p>
           </div>
           <div class="flex flex-col sm:mr-8">
-            <h3 class="font-semibold text-xl">Projets</h3>
+            <h3 class="font-semibold sm:text-xl text-lg">Projets</h3>
             <p class="text-white/70 mt-2">
               4eSport c'est également des projets tech au service de ses membres.
             </p>
           </div>
           <div class="flex flex-col">
-            <h3 class="font-semibold text-xl">Compétitions</h3>
+            <h3 class="font-semibold sm:text-xl text-lg">Compétitions</h3>
             <p class="text-white/70 mt-2">
               L'association permet à ses joueurs de participer à de grands tournois.
             </p>
@@ -80,13 +80,15 @@ import Footer from "../components/Footer.vue";
       </div>
       <article class="mt-20">
         <div class="flex flex-col text-center">
-          <h1 class="text-lg md:text-3xl font-semibold">Une association en 5 pôles</h1>
+          <h1 class="text-lg md:text-3xl font-semibold">Une association en 6 pôles</h1>
           <h2 class="text-base md:text-lg font-semibold text-[#15c584]">
             Quel que soit le niveau
           </h2>
         </div>
         <div>
-          <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-10 mt-10">
+          <div
+            class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 sm:gap-10 gap-6 mt-10"
+          >
             <div class="aspect-square">
               <img
                 src="../assets/lol.jpg"
@@ -127,6 +129,49 @@ import Footer from "../components/Footer.vue";
               />
               <h3 class="text-center text-white/60 mt-3">Rocket League</h3>
             </div>
+            <div class="aspect-square">
+              <img
+                src="../assets/rocket_league.jpg"
+                class="rounded-xl shadow-md shadow-black/50"
+                alt=""
+              />
+              <h3 class="text-center text-white/60 mt-3">Valorant</h3>
+            </div>
+          </div>
+        </div>
+      </article>
+      <article class="mt-20">
+        <div class="flex md:justify-between flex-col md:flex-row">
+          <div class="grid grid-cols-2 gap-12 order-1 m-auto">
+            <div class="flex flex-col text-center">
+              <p class="text-2xl">🚀</p>
+              <h4 class="font-semibold text-4xl">200+</h4>
+              <p class="text-xs text-white/50">membres en 2022</p>
+            </div>  
+            <div class="flex flex-col text-center">
+              <p class="text-2xl">🤗</p>
+              <h4 class="font-semibold text-4xl">1300+</h4>
+              <p class="text-xs text-white/50">utilisateurs sur notre Discord</p>
+            </div>
+            <div class="flex flex-col text-center">
+              <p class="text-2xl">🌟</p>
+              <h4 class="font-semibold text-4xl">1800+</h4>
+              <p class="text-xs text-white/50">points PAVE distribués</p>
+            </div>
+            <div class="flex flex-col text-center">
+              <p class="text-2xl">🤝</p>
+              <h4 class="font-semibold text-4xl">20+</h4>
+              <p class="text-xs text-white/50">équipes par an</p>
+            </div>
+          </div>
+          <div class="md:w-1/2 w-full mb-10 md:mb-0">
+            <h1 class="text-lg md:text-3xl font-semibold">Quelques chiffres</h1>
+            <h2 class="text-base md:text-lg font-semibold text-[#15c584]">
+              pour faire rêver
+            </h2>
+            <p class="text-white/70 text-justify md:text-left">4eSport est présente depuis 2019 au côté de ses membres. Elle a rapidement évoluée pour devenir la
+              deuxième plus grande association de l'école en 2022.
+            </p>
           </div>
         </div>
       </article>
