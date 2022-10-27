f049ee0ad49149668b7c3c140c727c27ace8504e
Mateleo <mateleo78@gmail.com>
2022-10-27 22:04:03 +0200

    Metrics

--- next/src/views/HomeView.vue
+++ next/src/views/HomeView.vue
@@ -2,9 +2,11 @@
 import Header from "../components/Header.vue"
 import Hero from "../components/Hero.vue"
 import Socials from "../components/Socials.vue"
+import Metrics from "../components/Metrics.vue"
 </script>
 <template>
   <Header></Header>
   <Hero></Hero>
   <Socials></Socials>
+  <Metrics></Metrics>
 </template>
