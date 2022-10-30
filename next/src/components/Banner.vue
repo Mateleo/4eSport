a2962fecf862f650bdf2d394a32114e71ff71881
Mateleo <mateleo78@gmail.com>
2022-10-30 12:51:06 +0100

    Banner

---
+++ next/src/components/Banner.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+<template>
+  <div class="bg-white text-black">
+    <div class="grid grid-cols-7 gap-6 text-4xl px-3 py-4 text-center">
+      <div v-for="x in 7" class="font-orbitron">
+        <p>4eSport</p>
+      </div>
+    </div>
+  </div>
+</template>
--- next/src/views/HomeView.vue
+++ next/src/views/HomeView.vue
@@ -3,10 +3,12 @@ import Header from "../components/Header.vue"
 import Hero from "../components/Hero.vue"
 import Socials from "../components/Socials.vue"
 import Metrics from "../components/Metrics.vue"
+import Banner from "../components/Banner.vue"
 </script>
 <template>
   <Header></Header>
   <Hero></Hero>
   <Socials></Socials>
   <Metrics></Metrics>
+  <Banner></Banner>
 </template>
