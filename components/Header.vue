e9e133c7dbddeebd2bd78fb86218b5f1d1a7c626
Paul HU <94650345+P4ND4P0W3R@users.noreply.github.com>
2024-09-10 14:41:46 +0200

    feat: Change header

--- components/Header.vue
+++ components/Header.vue
@@ -1,56 +1,33 @@
 <script setup lang="ts"></script>
 <template>
   <div class="fixed top-0 left-0 right-0 z-10">
-    <header
-      class="w-full border-b-[1px] border-white/20 backdrop-blur-[2px] text-white bg-[#1C1C1C]/70"
-    >
-      <a
-        href="https://discord.com/channels/252550911187091457/1210558515660329000"
-        target="_blank"
-        class="text-center w-full bg-green-600 font-bold text-xl flex justify-center py-1 text-white/90 hover:text-white transition-all ease-in-out"
-      >
-        Inscris-toi au tournoi 4eS ARENA du 6 septembre 2024 à 20h30 !
+    <header class="w-full border-b-[1px] border-white/20 backdrop-blur-[2px] text-white bg-[#1C1C1C]/70">
+      <a href="https://discord.com/channels/252550911187091457/1210558515660329000" target="_blank"
+        class="text-center w-full bg-green-600 font-bold text-xl flex justify-center py-1 text-white/90 hover:text-white transition-all ease-in-out">
+        Inscris-toi au tournoi 4eS LoL Cup du 13 septembre 2024 à 20h30 !
       </a>
-      <div
-        class="w-[95%] md:w-[90%] lg:w-[80%] max-w-[1500px] m-auto flex justify-between items-center py-1 md:py-2"
-      >
+      <div class="w-[95%] md:w-[90%] lg:w-[80%] max-w-[1500px] m-auto flex justify-between items-center py-1 md:py-2">
         <div class="items-center hidden md:flex">
           <div class="flex items-center">
-            <NuxtLink to="/" class="md:text-3xl text-sm font-bold"
-              >4eSport</NuxtLink
-            >
+            <NuxtLink to="/" class="md:text-3xl text-sm font-bold">4eSport</NuxtLink>
           </div>
         </div>
-        <nav
-          class="text-sm sm:text-base flex justify-between gap-3 sm:gap-8 m-auto md:m-0"
-        >
-          <NuxtLink
-            to="/"
-            class="block md:hidden link-underline hover:text-[#15c584] transition-all ease-in-out delay-75"
-          >
+        <nav class="text-sm sm:text-base flex justify-between gap-3 sm:gap-8 m-auto md:m-0">
+          <NuxtLink to="/"
+            class="block md:hidden link-underline hover:text-[#15c584] transition-all ease-in-out delay-75">
             4eSport
           </NuxtLink>
-          <NuxtLink
-            to="/projets"
-            class="link-underline hover:text-[#15c584] transition-all ease-in-out delay-75"
-          >
+          <NuxtLink to="/projets" class="link-underline hover:text-[#15c584] transition-all ease-in-out delay-75">
             Projets
           </NuxtLink>
-          <NuxtLink
-            to="/histoire"
-            class="link-underline hover:text-[#15c584] transition-all ease-in-out delay-75"
-          >
-            <span class="hidden md:inline-block">Notre</span> Histoire</NuxtLink
-          >
-          <NuxtLink
-            to="/cotisation"
-            class="link-underline hover:text-[#15c584] transition-all ease-in-out delay-75 flex"
-            >Cotisation
+          <NuxtLink to="/histoire" class="link-underline hover:text-[#15c584] transition-all ease-in-out delay-75">
+            <span class="hidden md:inline-block">Notre</span> Histoire
+          </NuxtLink>
+          <NuxtLink to="/cotisation"
+            class="link-underline hover:text-[#15c584] transition-all ease-in-out delay-75 flex">Cotisation
             <span class="relative inline-flex rounded-full h-2 w-2 bg-sky-500">
               <span
-                class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"
-              ></span
-            ></span>
+                class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span></span>
           </NuxtLink>
         </nav>
       </div>
