5c6e0863c6eb2208bc26e90d30a059bbb947bfff
Paul HU <94650345+P4ND4P0W3R@users.noreply.github.com>
2024-10-06 22:18:54 +0200

    feat: Update header
    Change from Fortnite to CS2

--- components/Header.vue
+++ components/Header.vue
@@ -2,9 +2,9 @@
 <template>
   <div class="fixed top-0 left-0 right-0 z-10">
     <header class="w-full border-b-[1px] border-white/20 backdrop-blur-[2px] text-white bg-[#1C1C1C]/70">
-      <a href="https://discord.com/channels/252550911187091457/367444313241550849/1288222151048171520" target="_blank"
+      <a href="https://discord.com/channels/252550911187091457/367444313241550849/1290753491886805023" target="_blank"
         class="text-center w-full bg-green-600 font-bold text-xl flex justify-center py-1 text-white/90 hover:text-white transition-all ease-in-out">
-        Inscris-toi au Tournoi 4eS Fortnite du 5 octobre 2024 à 16h00 !
+        Inscris-toi au Tournoi CS2 4eSport:STRIKE3 du 12 octobre 2024 à 13h00 !
       </a>
       <div class="w-[95%] md:w-[90%] lg:w-[80%] max-w-[1500px] m-auto flex justify-between items-center py-1 md:py-2">
         <div class="items-center hidden md:flex">
